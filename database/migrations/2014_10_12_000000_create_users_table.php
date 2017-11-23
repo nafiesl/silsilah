6f0a5f56bd4c649e79333373bf3279a1ce482044
Nafies Luthfi <nafiesl@gmail.com>
2017-11-23 17:43:39 +0800

    Fixes #1 Field 'manager_id' doesn't have a default value
    This fix makes `users`.`manager_id` nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
@@ -29,7 +29,7 @@ class CreateUsersTable extends Migration
             $table->string('address')->nullable();
             $table->string('city')->nullable();
             $table->string('phone')->nullable();
-            $table->unsignedInteger('manager_id');
+            $table->unsignedInteger('manager_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
