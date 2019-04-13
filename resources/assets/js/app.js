91c1c5650e3426c5105f9c0daf8d92490ebdaf09
Nafies Luthfi <nafiesl@gmail.com>
2019-04-13 22:16:26 +0800

    Remove Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,17 +6,3 @@
  */
 
 require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
