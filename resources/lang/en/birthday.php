2baecf260d8e593e67208931efc67a9bda7b2139
Nafies Luthfi <nafiesl@gmail.com>
2020-03-21 09:06:28 +0800

    Add en lang for birthday

---
+++ resources/lang/en/birthday.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'birthday'  => 'Birhtday',
+    'upcoming'  => 'Upcoming birthdays',
+    'remaining' => ':count days',
+    'age_years' => ':age years',
+    'days'      => 'days',
+];
